3e7d954397f977b81d08b1bc2b6a2d61cf76f5ee
MoRoPoPi <omarmandri26@gmail.com>
2024-11-09 14:09:44 +0100

    Update app title to 'QUICKQ App' in layout configuration

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,10 +5,7 @@ import { Metadata } from 'next';
 import { Nunito } from 'next/font/google';
 
 export const metadata: Metadata = {
-    title: {
-        template: '%s | VRISTO - Multipurpose Tailwind Dashboard Template',
-        default: 'VRISTO - Multipurpose Tailwind Dashboard Template',
-    },
+    title: 'QUICKQ App',
 };
 const nunito = Nunito({
     weight: ['400', '500', '600', '700', '800'],
